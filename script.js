a54a6e3455d31138243cb6e2f731f6a94097fd4c
Renan <111693987+Renanzim12@users.noreply.github.com>
2022-12-17 15:24:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.hamburguer').addEventListener('click', () => {
+
+    document.querySelector('.hamburguer').classList.toggle('menu');
+    document.querySelector('.links-header').classList.toggle('menu');
+})
